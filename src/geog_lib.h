face57e3c66cf1a26413bce1d881f9ee7071884e
tkgeomap <empty>
2008-08-09 16:58:48 +0000

    Initial revision

---
+++ src/geog_lib.h
@@ -0,0 +1,16 @@
+/*
+   geog_lib.h --
+
+   	This header declares structures and functions that store and
+	manipulate geographic data.
+ */
+
+#ifndef GEOG_H_
+#define GEOG_H_
+
+struct latlon {
+    unsigned lat;
+    unsigned lon;
+};
+
+#endif
