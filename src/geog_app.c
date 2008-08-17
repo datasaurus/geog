6591c1b3c1ecf1f1eb2293f495ad1ac57a9ec05a
tkgeomap <empty>
2008-08-17 04:15:09 +0000

    Initial revision

---
+++ src/geog_app.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "geog.h"
+
+int main(void)
+{
+    double l, r;
+
+    l = r = 0.0;
+    while (scanf(" r=%lf", &r) == 1 || scanf(" l=%lf", &l) == 1) {
+	printf("%g, r=%g => %g longitude\n", l, r, lonr(l, r));
+	printf("Enter a number: ");
+    }
+    return 0;
+}
---
+++ src/geog_lib.c
@@ -0,0 +1,8 @@
+#include <math.h>
+#include "geog.h"
+
+double lonr(double l, double r)
+{
+    l = fmod(l, 360.0);
+    return (l < r - 180.0) ? l + 360.0 : (l >= r + 180.0) ? l - 360.0 : l;
+}
