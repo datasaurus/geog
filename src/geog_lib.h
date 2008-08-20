f3d1b1653a98b34d044f8e750b05eea360531941
tkgeomap <empty>
2008-08-20 03:40:42 +0000

    (bug fix) Made an argument to rlon non-const

--- src/geog_lib.h
+++ src/geog_lib.h
@@ -4,12 +4,12 @@
    	This header declares structures and functions that store and
 	manipulate geographic data.
 
-	$Id: geog_lib.h,v 1.2 2008/08/17 04:14:08 tkgeomap Exp $
+	$Id: geog_lib.h,v 1.3 2008/08/20 03:39:16 tkgeomap Exp $
  */
 
 #ifndef GEOG_H_
 #define GEOG_H_
 
-double rlon(const double, const double);
+double rlon(double, const double);
 
 #endif
